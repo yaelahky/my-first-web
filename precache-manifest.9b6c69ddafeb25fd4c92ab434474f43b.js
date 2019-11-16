a245dac9a12a35b56bae513789e9b847ef04560c
ANANDA RIZKY Y <kiky.lenovo@gmail.com>
2019-11-16 13:50:12 +0700

    init

---
+++ precache-manifest.9b6c69ddafeb25fd4c92ab434474f43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e32decc64779b538a102dec2107853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27750a135a4e4b8890d9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2fae7386e5df350ced0b",
+    "url": "/static/js/2.2519c88d.chunk.js"
+  },
+  {
+    "revision": "27750a135a4e4b8890d9",
+    "url": "/static/js/main.7ab11272.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "245f98d6b2ca02c151eac685c17741e6",
+    "url": "/static/media/bg.245f98d6.png"
+  }
+]);
